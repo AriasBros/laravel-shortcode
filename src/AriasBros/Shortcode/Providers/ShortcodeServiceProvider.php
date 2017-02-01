963612ca712d79c98c01a058251ecf3a939d0852
David Arias Vázquez <david@MacBook-Air-de-David.local>
2017-02-01 22:58:27 +0100

    Fix to work with Laravel 5.4

--- src/AriasBros/Shortcode/Providers/ShortcodeServiceProvider.php
+++ src/AriasBros/Shortcode/Providers/ShortcodeServiceProvider.php
@@ -28,7 +28,7 @@ class ShortcodeServiceProvider extends ServiceProvider
         require_once(dirname(dirname(__FILE__)) . "/Helpers/shortcodes.php");
         require_once(dirname(dirname(__FILE__)) . "/Helpers/wordpress.php");
                 
-        $this->app->singleton("\AriasBros\Shortcode\Contracts\Factory", function ($app) {
+        $this->app->singleton("AriasBros\Shortcode\Contracts\Factory", function ($app) {
             return new \AriasBros\Shortcode\ShortcodeFactory();
         });                 
     }
